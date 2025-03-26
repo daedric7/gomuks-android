f186653fafa143967ee9402ead95396d0335397f
daedric7 <ricardo.dra.vieira@gmail.com>
2025-03-26 23:03:58 +0000

    Update NotificationChannels.kt

--- app/src/main/java/app/gomuks/android/NotificationChannels.kt
+++ app/src/main/java/app/gomuks/android/NotificationChannels.kt
@@ -1,6 +1,6 @@
 package app.gomuks.android
-
 import android.content.Context
+import android.net.Uri
 import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationManagerCompat
 
@@ -9,6 +9,8 @@ internal const val NOISY_NOTIFICATION_CHANNEL_ID = "noisy_notification"
 
 fun createNotificationChannels(context: Context) {
     val notificationManager = NotificationManagerCompat.from(context)
+    
+    // Create silent channel (unchanged)
     notificationManager.createNotificationChannel(
         NotificationChannelCompat.Builder(
             SILENT_NOTIFICATION_CHANNEL_ID,
@@ -22,6 +24,12 @@ fun createNotificationChannels(context: Context) {
             .build()
     )
 
+
+    
+    // Create noisy channel with bright sound (from gomuks web)
+    val resId = R.raw.bright
+    val soundUri = Uri.parse("android.resource://${context.packageName}/$resId")
+    
     notificationManager.createNotificationChannel(
         NotificationChannelCompat.Builder(
             NOISY_NOTIFICATION_CHANNEL_ID,
@@ -29,6 +37,7 @@ fun createNotificationChannels(context: Context) {
         )
             .setName(context.getString(R.string.notification_channel_noisy))
             .setDescription(context.getString(R.string.notification_channel_noisy))
+            .setSound(soundUri, null)
             .setVibrationEnabled(true)
             .setLightsEnabled(true)
             .setLightColor(R.color.primary_color)
