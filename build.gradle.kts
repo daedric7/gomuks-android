84616c78384b7f3433d9f332c00237fd9d2205f6
daedric7 <ricardo.dra.vieira@gmail.com>
2025-03-27 23:12:14 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,3 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.google.gms.google.services) apply false
 }
-
-repositories {
-    google()
-    mavenCentral()
-}
