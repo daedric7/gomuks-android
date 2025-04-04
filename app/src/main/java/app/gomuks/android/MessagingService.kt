b38c9258987a74574b07a9489777ecdfe3c27e9c
ricardo-duarte-av <ricardo.duarte@aguiarvieira.pt>
2025-04-05 00:33:05 +0100

    Update MessagingService.kt

--- app/src/main/java/app/gomuks/android/MessagingService.kt
+++ app/src/main/java/app/gomuks/android/MessagingService.kt
@@ -202,7 +202,7 @@ class MessagingService : FirebaseMessagingService() {
 
             // Create or update the conversation shortcut
 	    if (isGroupMessage) {
-		createOrUpdateGroupChatShortcut(this, data.roomID, roomName ?: roomAvatar, imageAuth)
+		createOrUpdateGroupChatShortcut(this, data.roomID, roomName ?: data.roomName, data.roomAvatar, data.imageAuth)
 	    } else {
 	    	createOrUpdateChatShortcut(this, data.roomID, roomName ?: data.sender.name, sender)
 	    }
